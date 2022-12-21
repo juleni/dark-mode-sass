ba9b6fd3c9603a3d7e80c46a74e818e478129068
juleni <69912179+juleni@users.noreply.github.com>
2022-12-21 14:03:44 +0100

    Added styles and button to switch dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import "./styles/_main.scss";
+
 function App() {
+  const THEME = ["light", "dark"];
+  const [theme, setTheme] = useState(THEME[0]);
+
+  function handleSetTheme() {
+    console.log("handleSetTheme");
+    console.log(theme);
+    if (theme === THEME[0]) {
+      setTheme(THEME[1]);
+    } else setTheme(THEME[0]);
+  }
   return (
-    <div className="App">
-      <h1>Dark Mode</h1>
+    <div className={theme}>
+      <div className="background flex">
+        <h1 className="text">Dark Mode</h1>
+
+        <button className="button" onClick={() => handleSetTheme()}>
+          Switch theme to {theme === THEME[0] ? THEME[1] : THEME[0]}
+        </button>
+      </div>
     </div>
   );
 }
